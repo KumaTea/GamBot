523f2e5cc093a45158aad1f557cdb5b6cbb18486
KumaTea <KumaTea@outlook.com>
2024-01-28 15:31:18 +0800

    fix epic free games

--- func/free/epic.py
+++ func/free/epic.py
@@ -1,12 +1,14 @@
 import json
 import aiohttp
+from datetime import datetime, timezone, timedelta
 
 
 EPIC_FREE_API = (
-    'https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?'
-    'locale=en-US&'
-    'country=US&'
-    'allowCountries=US'
+    'https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions'
+    # '?'
+    # 'locale=en-US&'
+    # 'country=US&'
+    # 'allowCountries=US'
 )
 
 
@@ -16,18 +18,53 @@ async def get_epic_free_games_json() -> dict:
             return json.loads(await resp.text())
 
 
+def get_game_link(game_json: dict) -> str:
+    if (
+        'productSlug' in game_json and
+        game_json['productSlug'] and
+        '-' in game_json['productSlug']
+    ):
+        return 'https://www.epicgames.com/store/en-US/p/' + game_json['productSlug']
+    if (
+        'offerMappings' in game_json and
+        game_json['offerMappings'] and
+        'pageSlug' in game_json['offerMappings'][0] and
+        '-' in game_json['offerMappings'][0]['pageSlug']
+    ):
+        return 'https://www.epicgames.com/store/en-US/p/' + game_json['offerMappings'][0]['pageSlug']
+    else:
+        return ''
+
+
 def epic_free_games_list(games_json: dict) -> list[dict]:
+    valid_games = []
     free_games = []
     games = games_json['data']['Catalog']['searchStore']['elements']
     for game in games:
-        if 'productSlug' in game and game['productSlug'] and '-' in game['productSlug']:
-            free_games.append(game)
+        if get_game_link(game):
+            valid_games.append(game)
+    for game in valid_games:
+        offers = game['promotions']['promotionalOffers']
+        if not offers:
+            continue
+        while 'promotionalOffers' in offers[0]:
+            offers = offers[0]['promotionalOffers']
+            if not offers:
+                continue
+        for offer in offers:
+            if offer['discountSetting']['discountPercentage'] != 0:
+                continue
+            start_date = datetime.fromisoformat(offer['startDate'])
+            end_date = datetime.fromisoformat(offer['endDate'])
+            now = datetime.now(timezone(timedelta(hours=8)))
+            if start_date <= now <= end_date:
+                free_games.append(game)
     return free_games
 
 
 def epic_game_info(game_json: dict) -> dict:
     game_title = game_json['title']
-    game_link = 'https://www.epicgames.com/store/en-US/p/' + game_json['productSlug']
+    game_link = get_game_link(game_json)
     # game_price = game_json['price']['totalPrice']['fmtPrice']['originalPrice']
     # game_appid = game_json['id']
     game_info = {
